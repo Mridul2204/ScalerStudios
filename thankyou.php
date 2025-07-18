d9d81aa3c47ec2febe61b29bf07fdb92da4eea20
saini1800 <sainideepak5701@gmail.com>
2025-07-18 08:38:42 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,37 @@
+<?php include 'header1.php';?>
+
+
+
+
+<div class=" service-main-section page-banner thankyou-messege ">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-lg-9 m-auto text-center">
+            <div class="main-heading thankyou-screen">
+                <!-- <p class="main-mini-heading">ABOUT US</p> -->
+               <h1 class="">Thank You for<span class="italic-font"> Booking Your Meeting!</span> </h1>
+               <p> We've received your booking and can't wait to connect!
+               </p>
+               <ol>
+                <li> <p> Confirmation email on the way. </p></li>
+                <li> <p> We'll discuss tailored strategies to boost your business. </p></li>
+                </ol>
+                <p>Need anything before your meeting?<br>
+                Reach us at <a href="mailto:contact@scalerstudios.com">contact@scalerstudios.com</a>
+                </p>
+            </div>
+         </div>
+      </div>
+    
+
+   </div>
+
+</div>
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
