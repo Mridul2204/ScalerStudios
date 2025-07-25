5e6215e2278484f21358eb3bbca93a296db2285b
saini1800 <sainideepak5701@gmail.com>
2025-07-25 23:39:10 +0530

    Create thank-you-header.php

---
+++ thank-you-header.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Scaler Studios</title>
+    
+      
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+      <!--=====CSS=======-->
+      <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/magnific-popup.css">
+      <link rel="stylesheet" href="css/nice-select.css">
+      <link rel="stylesheet" href="css/slick-slider.css">
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/aos.css">
+      <link rel="stylesheet" href="css/mobile-menu.css">
+      <link rel="stylesheet" href="css/main.css">
+      <link rel="stylesheet" href="css/style.css">
+
+      <link rel="stylesheet" href="css/responsive.css">
+      <!--=====JQUERY=======-->
+      <script src="js/jquery-3-6-0.min.js"></script>
+
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-XJBCHZNXWK"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-XJBCHZNXWK');
+      </script>
+
+
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-56H7RD4G');</script>
+<!-- End Google Tag Manager -->
+
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '643385051467272');
+fbq('track', 'PageView');
+fbq('track', 'BookMeeting')
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=643385051467272&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+
+
+
+      
+   </head>
+   <body class="body" >
+
+
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56H7RD4G"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+
+
+      
+      <header>
+         <div class="header-area header-area1 header-area-all d-none dark-font d-lg-block" id="header">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-12">
+                     <div class="header-elements">
+                        <div class="site-logo">
+                           <a href="index.php">
+                           <img src="img/green-logo.png" alt="" class="main-logo1">
+                           <img src="img/header-logo1.png" alt="" class="main-logo2">
+                           </a>
+                        </div>
+                        <div class="main-menu-ex main-menu-ex1">
+                           <ul>
+                           <li class="dropdown-menu-parrent
+                           "><a href="index.php" class="main1" >Home </a>
+</li>
+                           <li class="dropdown-menu-parrent
+                           "><a href="service.php" class="main1" >Services <i class="fa-solid fa-angle-down"></i> </a>
+                           <div class="tp-submenu">
+                           
+   
+      <div class="pricing-flexi-plans " >
+         <div class="row">
+            
+            <div class="col-sm-12">
+               <div class="pricing-flexi-box pricing-flexi-box2 pricing-flexi-box3" >
+                  <div class="row">
+                     <div class="col-md-4">
+                        <div class="pricing-box-content">
+                           <div class="row">
+                              <div class="col-10">
+                                 <div class="main-heading">
+                                    <span class="span" style="background-color:#d8ff85;">Creative Design Services </span>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="plans-row">
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Video Editing</p>
+                                       <p>Transform Footage, tell your story.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Motion Design</p>
+                                       <p>Bring Visuals to life, captivate viewers.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Social Media Creative</p>
+                                       <p>Craft engaging content, boost your presence.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Graphic Design</p>
+                                       <p>Create impactful visuals, communicate clearly.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Ad Creative</p>
+                                       <p>Design compelling ads, maximize conversions.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p> Print Design</p>
+                                       <p> Produce memorable prints, leave a lasting impression</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Web Design</p>
+                                       <p>Build user-friendly sites, drive online growth.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-md-4">
+                        <div class="pricing-box-content">
+                           <div class="row">
+                              <div class="col-10">
+                                 <div class="main-heading">
+                                    <span class="span" style="background-color:#011c1f; color:#fff;" >AI Services </span>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="plans-row">
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>AI Enhance Creative</p>
+                                       <p>Supercharge creativity, unlock AI-Powerwd Innovation.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>AI Consulting</p>
+                                       <p>Optimize operations, implement intelligent solutions.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-md-4">
+                        <div class="pricing-box-content">
+                           <div class="row">
+                              <div class="col-10">
+                                 <div class="main-heading">
+                                    <span class="span"  style="background-color:#19546d; color:#fff;">Marketing Services</span>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="plans-row">
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Marketing Strategy</p>
+                                       <p>Data-driven plans, accelerate business growth.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Google Ads</p>
+                                       <p>Instant visibility, drive targeted website traffic.</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="plans-box">
+                                 <div class="row">
+                                    <div class="col-10">
+                                       <p>Facebook Ads</p>
+                                       <p>Precision targeting, maximize social ROI</p>
+                                    </div>
+                                    <div class="col-2">
+                                       <p class="text-right"><i class="fa-solid fa-check"></i></p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                 
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+                           
+
+                           
+                           </li>
+                           <li class="dropdown-menu-parrent"><a href="portfolio.php" class="main1" >Our Work</a></li>
+                           
+                           <li class="dropdown-menu-parrent"><a href="Pricing.php" class="main1" >Pricing</a></li>
+                              <li><a href="about-us.php">About Us</a></li>
+                              <!-- <li class="dropdown-menu-parrent"><a href="portfolio.php" class="main1"> Portfolio </a>
+                              </li> -->
+                              <li class="dropdown-menu-parrent"><a href="enterprise.php" class="main1" >Enterprise </a></li>
+                              
+                           </ul>
+                        </div>
+                        <div class="header2-buttons">
+                           <div class="button">
+                           <a class="theme-btn1" href="javascript:void(0)" id="openPopupBtn" >Get Started</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!--=====HEADER END=======-->
+      <!--=====Mobile header start=======-->
+      <div class="mobile-header mobile-header-main d-block d-lg-none dark-bg ">
+         <div class="container-fluid">
+            <div class="col-12">
+               <div class="mobile-header-elements">
+                  <div class="mobile-logo">
+                     <a href="index.php">
+                     <img src="img/green-logo.png" alt="" class="main-logo1">
+                           <img src="img/header-logo1.png" alt="" class="main-logo2"></a>
+                  </div>
+                  <div class="mobile-nav-icon">
+                  <i class="fa-solid fa-bars-staggered"></i>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="mobile-sidebar d-block d-lg-none">
+         <div class="logo-m">
+            <a href="index.html"><img src="img/header-logo1.png" alt=""></a>
+         </div>
+         <div class="menu-close">
+            <i class="fa-solid fa-xmark"></i>
+         </div>
+         <div class="mobile-nav">
+            <ul>
+               <li class="has-dropdown"><a href="service.php">Service</a></li>
+               <li class="has-dropdown"><a href="portfolio.php">Our Work</a></li>
+               <li><a href="Pricing.php">Pricing</a></li>
+               <li><a href="about-us.php">About Us</a></li>
+               <!-- <li class="dropdown-menu-parrent"><a href="enterprise.php" class="main1" >Enterprise </a></li> -->
+                <li class="dropdown-menu-parrent"><a href="enterprise.php" class="main1" >Enterprise </a></li>
+            </ul>
+         </div>
+         <div class="header2-buttons">
+                           <div class="button">
+                           <a class="theme-btn1" href="javascript:void(0)" id="openPopupBtn" >Get Started</a>
+                           </div>
+                        </div>
+      </div>
+      <!--=====Mobile header end=======-->
